bfa7b76b5bb4f29c5183642414dff75a44482302
Przemysław Curzytek <curzytekp@gmail.com>
2019-09-16 19:53:25 -0400

    leave just game page

--- client/index.js
+++ client/index.js
@@ -1,26 +1,62 @@
 // var siteDomain = "https://" + document.domain;
 var siteDomain = "http://localhost:7071";
 
+var gameId;
+
 $(document).ready(function () {
 
-});
+    gameId = $.urlParam('id');
 
-$('#newGame').click(function (event) {
-    event.preventDefault();
-
-    $.ajax({
-        url: siteDomain + "/creategame",
-        type: "POST",
-        contentType: "application/json",
-        dataType: "json",
-        success: function (data) {
-            $('#newGameLink a').attr('href', `/game.html?id=${data.id}`);
-
-            // display URL to game's site
-            $('#newGameLink').collapse('show');
-        },
-        error: function (_, textStatus) {
-            console.log(textStatus)
-        }
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = $('.needs-validation');
+    Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
     });
 });
+
+$('#joinGameForm').submit(function (event) {
+    event.preventDefault();
+
+    if ($.trim($('#playerName').val()) != '') {
+
+        var joinGameData = {
+            "playerName": $('#playerName').val(),
+            "gameId": gameId
+        };
+
+        $.ajax({
+            url: siteDomain + "/joingame",
+            type: "POST",
+            data: JSON.stringify(joinGameData),
+            contentType: "application/json",
+            dataType: "json",
+            success: function (data) {
+                $('#yourGameLink a').attr('href', `/answer.html?playerId=${data.playerId}`);
+
+                $('#joinGameForm input').prop('disabled', true);
+                $('#joinGameForm button').prop('disabled', true);
+
+                // display URL to game's site
+                $('#yourGameLink').collapse('show');
+            },
+            error: function (_, textStatus) {
+                console.log(textStatus)
+            }
+        });
+    }
+});
+
+$.urlParam = function (name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results == null) {
+        return null;
+    } else {
+        return results[1] || 0;
+    }
+}
